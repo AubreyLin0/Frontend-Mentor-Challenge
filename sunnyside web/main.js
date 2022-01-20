e52f65eb729e53b3493046b57cc161afbf5f4315
Aubrey1217 <91876438+Aubrey1217@users.noreply.github.com>
2022-01-20 16:28:58 +0900

    delete the useless part

--- sunnyside web/main.js
+++ sunnyside web/main.js
@@ -1,5 +1,4 @@
 var prepend = true
-// var display = true
 $('.navbarToggler').click(function () {
   const triangle = "<div id='triangle-bottomright'></div>"
   if (prepend) {
@@ -7,12 +6,5 @@ $('.navbarToggler').click(function () {
     prepend = false
   }
   $('.navbarContent').slideToggle('100')
-  // if (display) {
-  //   $('.navbarContent').css({ display: 'none' })
-  //   display = false
-  // }
-  // $('.navbarContent').toggle(function () {
-  //   $(this).css({ display: 'none' })
-  // })
   $('.navbarContent').toggleClass('show-links')
 })
